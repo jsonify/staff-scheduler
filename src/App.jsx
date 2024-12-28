58f8a83f3571f6a95ded8aaaa9bbe6189dd9c276
jsonify <phlacin@gmail.com>
2024-12-28 13:45:48 -0800

    chore: Remove unused files and reset project structure

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Hello World!</h1>
-      <p className="mt-2">Let's build something great!</p>
-    </div>
-  )
-}
