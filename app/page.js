407131d67b7b622958cd4b2d88b8fdd0e61dce92
jsonify <phlacin@gmail.com>
2024-12-29 11:08:18 -0800

    feat: Add app/page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SubstituteCalendar from "../components/SubstituteCalendar";
+
+export default function Home() {
+  return (
+    <div>
+      <SubstituteCalendar />
+    </div>
+  );
+}
